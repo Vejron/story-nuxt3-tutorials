9ac949d3bd4453bff90a3f552c46a67da5568ff5
vejron <bjorn.yttergren@gmail.com>
2022-06-13 13:41:36 +0200

    wip

--- storyblok/Teaser.vue
+++ storyblok/Teaser.vue
@@ -1,13 +1,18 @@
 <template>
-  <div v-editable="blok" class="sm:text-center">
-    <Component :is="blok.level ?? 'h3'" class="text-4xl sm:text-6xl font-black">{{
-      blok.heading
-    }}</Component>
-    <p class="max-w-xl text-lg my-6 mx-auto ">{{ blok.tagline }}</p>
+  <div v-editable="blok" :class="{'sm:text-center': blok.centered}">
+    <Component
+      :is="blok.level ?? 'h2'"
+      class="text-2xl sm:text-4xl font-bold"
+      >{{ blok.heading }}</Component
+    >
+    <div class="prose prose-slate text-lg max-w-xl" :class="{'sm:mx-auto': blok.centered}">
+      <rich-text-renderer :document="blok.content" />
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { RichTextRenderer } from "@marvr/storyblok-rich-text-vue-renderer";
 const props = defineProps({
   blok: {
     type: Object,
