026013ff296517daf4787328e21b4b5c42ab4b7f
vejron <bjorn.yttergren@gmail.com>
2022-06-13 12:31:49 +0200

    wip: try prerender

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,12 @@ import fs from "fs";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  ssr: true,
+  nitro: {
+    prerender: {
+      crawlLinks: true
+    }
+  },
   vite: {
     server: {
       https: {
